a7199752d3b958ed85e7c0193337a37d555742cd
lacoca <032287d@utea.edu.pe>
2023-08-23 07:39:56 -0500

    MULTIPLICAR

---
+++ PRACTICANDO/funciones.dart
@@ -0,0 +1,27 @@
+void main(List<String> args) {
+  mostrarMensaje();
+  mostrarMensaje();
+  mostrarMensaje();
+  //suma();
+  print(suma(5,2));
+ }
+mostrarMensaje(){
+
+  print("hola mundo!!!desde una funcion");
+}
+/*void suma(){// acepta void por que s una funcion sin retorno
+  print("la suma es: ${2+3}");
+}*/
+/*double suma(){
+
+  return 2+2;
+
+}*/
+/*double suma(int n1){
+  return n1+2;
+}*/
+int suma(int n1, int n2){
+print("la suma es");
+  return n1+n2;
+}
+  
---
+++ PRACTICANDO/multiplicar.dart
@@ -0,0 +1,15 @@
+// Función para imprimir una tabla de multiplicar
+void tablaDeMultiplicar(int numero, int limite) {
+  print("Tabla de multiplicar del $numero:");
+  for (int i = 1; i <= limite; i++) {
+    int resultado = numero * i;
+    print("$numero x $i = $resultado");
+  }
+}
+
+void main() {
+  int numero = 7; // El número para el que deseamos crear la tabla de multiplicar
+  int limite = 10; // El límite hasta el cual deseamos multiplicar
+
+  tablaDeMultiplicar(numero, limite);
+}
