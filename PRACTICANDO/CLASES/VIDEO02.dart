206e5240cbc2ace63c60430200bd637c7dced802
lacoca <032287d@utea.edu.pe>
2023-08-24 22:27:15 -0500

    VIDEO 02

---
+++ PRACTICANDO/CLASES/VIDEO02.dart
@@ -0,0 +1,27 @@
+void main(){
+
+final Hero wolverine=Hero(name: 'Logan' ,power:'Regeneracion');
+
+//Se crea una instancia de la clase Hero llamada 
+//wolverine usando el constructor. Se le asigna el 
+//nombre 'Logan' y el poder 'Regeneracion'
+
+print(wolverine); //imprime la instancia de la clase wolverine
+print(wolverine.name);
+print(wolverine.power);
+
+}
+class Hero{  //Aquí se define una clase llamada Hero
+String name; //propiedades name y es de tipo string
+String power; // propiedad power y es de tipo string
+
+Hero({
+  required this.name,
+  required this.power
+});
+
+//Hero(String pname,String ppower) //se define un constructor llamado Hero
+//: name=pname,// el constructor, se asigna el valor de pname a la propiedad name
+//power=ppower;// el valor de ppower asigna a la propiedad power
+//Hero(this.name, this.power); // que tambien podemos simplifica en una linea 
+}
